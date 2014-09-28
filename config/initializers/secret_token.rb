8febbad528380ddfc411beb5e6afe061c02f79c1
Johan Tino Frederiksen <johantino@gmail.com>
2014-09-28 15:58:18 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tinkuy::Application.config.secret_key_base = 'aef812cffa7f4e9baa73bb2daba0f3c5bf915f09dab380142d393202454fdd0bbbeecc4fc08591fd839eb9064e12dc7de1596b88ad8ce3b2b058067de6d7e3f5'
