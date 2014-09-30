383b4eb687d0dad119786084d62e0382b8061c23
Karsten Bjerring Olsen <kbjerring@gmail.com>
2014-09-30 17:49:08 +0200

    added simple front page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Tinkuy::Application.routes.draw do
   devise_for :users
+
   resources :events
-  root :to => "events#index"
+
+  root :to => "pages#index"
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
