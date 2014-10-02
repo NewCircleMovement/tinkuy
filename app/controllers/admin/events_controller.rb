74ac7459420d70847419ef685bcaf38562df3b80
Karsten Bjerring Olsen <kbjerring@gmail.com>
2014-10-02 11:33:34 +0200

    admin can confirm/unconfirm/delete/edit events, admin nav now indicates current page

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -1,8 +1,48 @@
 class Admin::EventsController < Admin::BaseController
+  before_action :set_event, only: [:edit, :update, :destroy]
 
   def index
     @confirmed_events = Event.where(:confirmed => true).order(startdate: :asc, starttime: :asc)
     @unconfirmed_events = Event.where(:confirmed => false).order(startdate: :asc, starttime: :asc)
   end
+
+  # GET /events/1/edit
+  def edit
+  end
+
+  # PATCH/PUT /events/1
+  # PATCH/PUT /events/1.json
+  def update
+    respond_to do |format|
+      if @event.update(event_params)
+        format.html { redirect_to admin_events_path, notice: 'Aktiviteten er nu blevet opdateret.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /events/1
+  # DELETE /events/1.json
+  def destroy
+    @event.destroy
+    respond_to do |format|
+      format.html { redirect_to admin_events_url }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_event
+      @event = Event.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def event_params
+      params.require(:event).permit(:name, :description, :duration, :starttime, :startdate, :confirmed)
+    end  
 
 end
