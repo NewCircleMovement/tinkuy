c87be57d010bd5ddc20f269029b61801dd271f4c
Karsten Bjerring Olsen <kbjerring@gmail.com>
2014-11-03 17:57:41 +0100

    user names sorted by name, and titleized

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
   
   def index
-    @confirmed_users = User.where(:active => true)
-    @pending_users = User.where(:pending => true)
+    @confirmed_users = User.where(:active => true).order(:firstname, :surname)
+    @pending_users = User.where(:pending => true).order(:firstname, :surname)
   end
 
 end
