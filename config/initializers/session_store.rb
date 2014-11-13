41f8d03b32417fd99ec62267c4a643eda5251b45
Karsten Bjerring Olsen <kbjerring@gmail.com>
2014-11-13 10:27:38 +0100

    fixed time_save in user session

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-Tinkuy::Application.config.session_store :active_record_store
+Tinkuy::Application.config.session_store :active_record_store, :expire_after => 2.hours
+
 # Tinkuy::Application.config.session_store :cookie_store, key: '_tinkuy_session'
