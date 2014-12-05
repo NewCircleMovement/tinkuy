ff2127a6d3a9a79fdc0926d9da1790ad5d498559
Karsten Bjerring Olsen <kbjerring@gmail.com>
2014-12-05 08:59:18 +0100

    added send grid mailer for production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,18 @@ Tinkuy::Application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = { :host => "tinkuy.dk" }
+  
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com'
+  }
+  ActionMailer::Base.delivery_method = :smtp
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
   config.i18n.fallbacks = true
