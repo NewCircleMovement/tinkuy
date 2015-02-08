78d31906dbadae5a59ceaf7b0f9ece94e48adffc
Karsten Bjerring Olsen <kbjerring@gmail.com>
2015-02-08 15:21:46 +0100

    test other way to set api keys

--- config/initializers/koudoku.rb
+++ config/initializers/koudoku.rb
@@ -4,17 +4,21 @@ Koudoku.setup do |config|
 
   if Rails.env == "production"
     # set credentials from ENV hash
-    STRIPE_CREDENTIALS = { 
-      :stripe_publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
-      :stripe_secret_key => ENV['STRIPE_SECRET_KEY']
-    }
+    # STRIPE_CREDENTIALS = { 
+    #   :stripe_publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+    #   :stripe_secret_key => ENV['STRIPE_SECRET_KEY']
+    # }
+    config.stripe_publishable_key = ENV[:STRIPE_PUBLISHABLE_KEY]
+    config.stripe_secret_key = ENV[:STRIPE_SECRET_KEY]
   else
     # get credentials from YML file
     STRIPE_CREDENTIALS = YAML.load_file(Rails.root.join("config/stripe.yml"))[Rails.env].try(:symbolize_keys)
+    config.stripe_publishable_key = STRIPE_CREDENTIALS[:STRIPE_PUBLISHABLE_KEY]
+    config.stripe_secret_key = STRIPE_CREDENTIALS[:STRIPE_SECRET_KEY]
   end
 
-  config.stripe_publishable_key = STRIPE_CREDENTIALS[:STRIPE_PUBLISHABLE_KEY]
-  config.stripe_secret_key = STRIPE_CREDENTIALS[:STRIPE_SECRET_KEY]
+  # config.stripe_publishable_key = STRIPE_CREDENTIALS[:STRIPE_PUBLISHABLE_KEY]
+  # config.stripe_secret_key = STRIPE_CREDENTIALS[:STRIPE_SECRET_KEY]
 
   # config.prorate = false # Default is true, set to false to disable prorating subscriptions
   # config.free_trial_length = 30
