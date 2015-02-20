1b6cabf5cc69adc48d2a279a1b983898cb459e6b
Karsten Bjerring Olsen <kbjerring@gmail.com>
2015-02-20 15:53:00 +0100

    prevent new users

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,3 +1,10 @@
 class RegistrationsController < Devise::RegistrationsController
+  before_filter :check_if_full
+
+  def check_if_full
+    if @places_left <= 0
+      redirect_to root_url, :notice => "Der er ingen pladser tilbage"
+    end
+  end
 
 end
