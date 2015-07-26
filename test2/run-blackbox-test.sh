09ff11764bd479cafccacf01f75c7184a5d76238
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2015-07-26 17:28:15 +0200

    blackbox testing on travis in progress

--- test2/run-blackbox-test.sh
+++ test2/run-blackbox-test.sh
@@ -1,19 +1,19 @@
-dropdb tinkuy_development
-createdb tinkuy_development
-xzcat test2/sample-db.pgsql.xz | psql tinkuy_development
-gem install rails
 rails server &
 sleep 10
 rm -rf test2/result; install -d test2/result; cd test2/result
 for x in \
+  "/"
   "/events?dato=2015-07-06" \
   "/users" \
   "/resources" \
   "/info" 
 do
   echo $x
-  curl -s "http://localhost:3000$x" | grep -v token > ".$x"
+  curl -s "http://localhost:3000$x" | grep -v token > ".$x-out"
 done
 
+ls -l
 cd ..
-diff -r expected result
+killall ruby
+sleep 1
+# diff -r expected result
