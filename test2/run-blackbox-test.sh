3e72d6871cedd384a9f6831d9d98da6012a4497a
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2015-07-26 19:19:27 +0200

    run rails server in test-mode during testing

--- test2/run-blackbox-test.sh
+++ test2/run-blackbox-test.sh
@@ -1,4 +1,7 @@
-rails server &
+dropdb tinkuy_test
+createdb tinkuy_test
+xzcat test2/sample-db.pgsql.xz | psql tinkuy_test
+RAILS_ENV="test" rails server &
 sleep 10
 rm -rf test2/result; install -d test2/result; cd test2/result
 for x in \
