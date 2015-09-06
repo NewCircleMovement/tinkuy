54820a9a99f06953ef62f0af5925bc1414fc18bc
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2015-09-06 22:38:16 +0200

    add more info to show.html (displayed event), fixes #57

--- test2/run-blackbox-test.sh
+++ test2/run-blackbox-test.sh
@@ -5,14 +5,15 @@ RAILS_ENV="test" rails server &
 sleep 10
 rm -rf test2/result; install -d test2/result; cd test2/result
 for x in \
-  "/" \
-  "/events?dato=2015-07-06" \
-  "/users" \
-  "/resources" \
-  "/info" 
+  "" \
+  "events?dato=2015-07-06" \
+  "users" \
+  "events/1111" \
+  "resources" \
+  "info" 
 do
   echo $x
-  curl -s "http://localhost:3000$x" | grep -v token > ".$x-out"
+  curl -s "http://localhost:3000/$x" | grep -v token > "./`echo $x | tr /? __`-out"
 done
 
 ls -l
