cbfe50718365e2be8eaafdfc70ed859614481131
Karsten Bjerring Olsen <kbjerring@gmail.com>
2016-03-12 16:39:07 +0100

    fix possibility for support membership even if there are no places left

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,17 @@
 class RegistrationsController < Devise::RegistrationsController
   before_filter :get_plan, :only => [:new]
-  before_filter :check_if_full
+  # before_filter :check_if_full
 
   def get_plan
     if session["new_plan"].present?
       @plan = Plan.find(session["new_plan"].to_i)
+
+      if @places_left == false
+        unless @plan.membership.name == 'SUPPORTER'
+          redirect_to root_url, :notice => "Der er ingen pladser tilbage"
+        end
+      end  
+    
     end
   end
 
@@ -15,12 +22,12 @@ class RegistrationsController < Devise::RegistrationsController
   #   redirect_to owner_subscription_path(@owner, @subscription)
   # end
 
-  def check_if_full
-    if @places_left == false
-      unless @plan.membership.name == 'SUPPORTER'
-        redirect_to root_url, :notice => "Der er ingen pladser tilbage"
-      end
-    end
-  end
+  # def check_if_full
+  #   # if @places_left == false
+  #     unless @plan.membership.name == 'SUPPORTER'
+  #       redirect_to root_url, :notice => "Der er ingen pladser tilbage"
+  #     end
+  #   # end
+  # end
 
 end
