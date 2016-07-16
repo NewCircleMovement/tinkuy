e60a30307a17c50e52266812d278e3881130aa54
Anders Thøgersen <anders@bladre.dk>
2016-07-16 16:38:09 +0200

    db/schema.rb changed due to rails 2.2.3 --> rails 2.2.5 upgrade
    This doesn't look dangerous (famous last words...)
    
    See: http://stackoverflow.com/questions/27697119/what-is-cascade-in-rails-schema-rb-and-where-did-it-come-from

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 20160205085559) do
   enable_extension "plpgsql"
   enable_extension "pg_stat_statements"
 
-  create_table "bids", force: true do |t|
+  create_table "bids", force: :cascade do |t|
     t.integer  "event_id"
     t.integer  "user_id"
     t.integer  "amount"
@@ -26,29 +26,29 @@ ActiveRecord::Schema.define(version: 20160205085559) do
     t.datetime "updated_at"
   end
 
-  create_table "bookings", force: true do |t|
+  create_table "bookings", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "timeslot_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "centers", force: true do |t|
-    t.string   "name"
+  create_table "centers", force: :cascade do |t|
+    t.string   "name",        limit: 255
     t.integer  "max_members"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "coupons", force: true do |t|
-    t.string   "code"
-    t.string   "free_trial_length"
+  create_table "coupons", force: :cascade do |t|
+    t.string   "code",              limit: 255
+    t.string   "free_trial_length", limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "events", force: true do |t|
-    t.string   "name"
+  create_table "events", force: :cascade do |t|
+    t.string   "name",         limit: 255
     t.text     "description"
     t.time     "starttime"
     t.integer  "week_number"
@@ -58,22 +58,22 @@ ActiveRecord::Schema.define(version: 20160205085559) do
     t.integer  "duration"
     t.integer  "hour"
     t.integer  "minute"
-    t.boolean  "confirmed",    default: false
+    t.boolean  "confirmed",                default: false
     t.integer  "user_id"
     t.integer  "fruits_count"
     t.integer  "max_spaces"
     t.integer  "free_spaces"
   end
 
-  create_table "fruitbaskets", force: true do |t|
+  create_table "fruitbaskets", force: :cascade do |t|
     t.integer  "owner_id"
-    t.string   "owner_type"
+    t.string   "owner_type",   limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "fruits_count", default: 0
+    t.integer  "fruits_count",             default: 0
   end
 
-  create_table "fruits", force: true do |t|
+  create_table "fruits", force: :cascade do |t|
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -81,21 +81,21 @@ ActiveRecord::Schema.define(version: 20160205085559) do
     t.integer  "fruitbasket_id"
   end
 
-  create_table "memberships", force: true do |t|
-    t.string   "name"
+  create_table "memberships", force: :cascade do |t|
+    t.string   "name",             limit: 255
     t.integer  "center_id"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "max_members"
     t.integer  "price"
-    t.integer  "fruits_per_month", default: 0
+    t.integer  "fruits_per_month",             default: 0
   end
 
-  create_table "plans", force: true do |t|
-    t.string   "name"
-    t.string   "stripe_id"
+  create_table "plans", force: :cascade do |t|
+    t.string   "name",               limit: 255
+    t.string   "stripe_id",          limit: 255
     t.float    "price"
-    t.string   "interval"
+    t.string   "interval",           limit: 255
     t.text     "features"
     t.boolean  "highlight"
     t.integer  "display_order"
@@ -108,7 +108,7 @@ ActiveRecord::Schema.define(version: 20160205085559) do
     t.integer  "membership_id"
   end
 
-  create_table "recurring_bookings", force: true do |t|
+  create_table "recurring_bookings", force: :cascade do |t|
     t.integer  "day"
     t.time     "time"
     t.integer  "user_id"
@@ -118,16 +118,16 @@ ActiveRecord::Schema.define(version: 20160205085559) do
     t.integer  "resource_id"
   end
 
-  create_table "resources", force: true do |t|
-    t.string   "name"
+  create_table "resources", force: :cascade do |t|
+    t.string   "name",        limit: 255
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "duration"
   end
 
-  create_table "sessions", force: true do |t|
-    t.string   "session_id", null: false
+  create_table "sessions", force: :cascade do |t|
+    t.string   "session_id", limit: 255, null: false
     t.text     "data"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -136,12 +136,12 @@ ActiveRecord::Schema.define(version: 20160205085559) do
   add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", unique: true, using: :btree
   add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at", using: :btree
 
-  create_table "subscriptions", force: true do |t|
-    t.string   "stripe_id"
+  create_table "subscriptions", force: :cascade do |t|
+    t.string   "stripe_id",          limit: 255
     t.integer  "plan_id"
-    t.string   "last_four"
+    t.string   "last_four",          limit: 255
     t.integer  "coupon_id"
-    t.string   "card_type"
+    t.string   "card_type",          limit: 255
     t.float    "current_price"
     t.integer  "user_id"
     t.datetime "created_at"
@@ -150,7 +150,7 @@ ActiveRecord::Schema.define(version: 20160205085559) do
     t.date     "old_plan_exit_date"
   end
 
-  create_table "timeslots", force: true do |t|
+  create_table "timeslots", force: :cascade do |t|
     t.integer  "resource_id"
     t.date     "startdate"
     t.time     "starttime"
@@ -162,33 +162,33 @@ ActiveRecord::Schema.define(version: 20160205085559) do
     t.boolean  "is_recurring", default: false
   end
 
-  create_table "users", force: true do |t|
-    t.string   "email",                             default: "",        null: false
-    t.string   "encrypted_password",                default: "",        null: false
-    t.string   "reset_password_token"
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  limit: 255, default: "",        null: false
+    t.string   "encrypted_password",     limit: 255, default: "",        null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                     default: 0,         null: false
+    t.integer  "sign_in_count",                      default: 0,         null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "firstname"
-    t.string   "surname"
-    t.boolean  "confirm_payment",                   default: false
-    t.string   "status",                 limit: 50, default: "pending"
-    t.boolean  "admin",                             default: false
+    t.string   "firstname",              limit: 255
+    t.string   "surname",                limit: 255
+    t.boolean  "confirm_payment",                    default: false
+    t.string   "status",                 limit: 50,  default: "pending"
+    t.boolean  "admin",                              default: false
     t.integer  "membertype"
     t.integer  "membership_id"
-    t.boolean  "admin_info",                        default: false
+    t.boolean  "admin_info",                         default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
-  create_table "votes", force: true do |t|
+  create_table "votes", force: :cascade do |t|
     t.integer  "event_id"
     t.integer  "user_id"
     t.datetime "created_at"
